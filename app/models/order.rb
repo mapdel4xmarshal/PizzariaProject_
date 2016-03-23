3301be9d0c9d9696f7c3e32f2c85fafc01a82b46
mapdel4xmarshal <mapdel4xmarshal@gmail.com>
2016-03-23 04:02:30 +0000

    I finished coding the orders validations as well as pizza price calculation. I also modified the orders table. I started working on the users views

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,5 +15,67 @@ class Order < ActiveRecord::Base
 
     canadian_phone = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
     validates :Phone, format: { with: canadian_phone, :multiline => true}
+    
+    validates :Location, :presence => true
+    
+     
+    def Price 
+      @SizePrice = 0
+      if self.Size == "Small" 
+         @SizePrice = 5.00
+         
+      elsif self.Size == "Medium" 
+         @SizePrice = 10.00
+         
+      elsif self.Size == "Large" 
+         @SizePrice = 15.00
+         
+      elsif self.Size == "Extra Large" 
+         @SizePrice = 20.00
+      end 
+      
+      if (attribute_present?('Toppings') && self.Crust == "Stuffed")
+         @SizePrice += 2
+      end
+      
+      if attribute_present?('Toppings')
+          if(self.Toppings.count(',') > 0)
+             @SizePrice += (self.Toppings.count(',') * 0.5)
+          end
+      end
+      
+      if(attribute_present?('quantity') && (self.quantity.is_a? Numeric))
+          @SizePrice *= self.quantity
+      end
+      
+      $prePrice = @SizePrice
+      
+      if(attribute_present?('Location'))
+          if self.Location == "Manitoba"
+              @SizePrice += (@SizePrice * 0.10) 
+                $Tax = 0.10
+          end
+          
+          if self.Location == "Ontario"
+              @SizePrice += (@SizePrice * 0.13) 
+              $Tax = 0.13
+          end
+          
+          if self.Location == "Quebec"
+              @SizePrice += (@SizePrice * 0.11) 
+              $Tax = 0.11
+          end
+          
+          if self.Location == "Saskatchewan"
+              @SizePrice += (@SizePrice * 0.15) 
+              $Tax = 0.15
+          end
+      end
+      
+      $Tax = $prePrice * $Tax
+      
+      return @SizePrice
+    end 
+
 end
     
